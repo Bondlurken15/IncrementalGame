c553635d7d61d5633e1b204563d2d4b795448268
Leo Shafiee <leo.shafiee@learnet.se>
2024-01-22 10:56:40 +0100

    autoclicker

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -2,15 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Clicker : MonoBehaviour
 {
+    [SerializeField] bool isAutoClick;
+    [SerializeField] float autoClickInterval = 1.0f;
+    [SerializeField] int autoClickAmount = 1;
     int clickCounter = 0;
     [SerializeField] TextMeshProUGUI clickCounterText;
-    
+    [SerializeField] Button autoClickButton;
+
+    private bool autoClickRunning;
+
+    private void Start()
+    {
+        autoClickRunning = isAutoClick;
+        if (isAutoClick)
+        {
+            StartCoroutine(AutoClickCoroutine());
+        }
+
+        // Attach the button click event handler
+        autoClickButton.onClick.AddListener(ToggleAutoClick);
+    }
+
+    private void Update()
+    {
+      
+
+        // Check if the state of isAutoClick has changed
+        if (isAutoClick != autoClickRunning)
+        {
+            autoClickRunning = isAutoClick;
+
+            if (autoClickRunning)
+            {
+                StartCoroutine(AutoClickCoroutine());
+            }
+            else
+            {
+                StopCoroutine(AutoClickCoroutine());
+            }
+        }
+    }
+
     public void OnClick()
     {
         clickCounter++;
         clickCounterText.text = clickCounter.ToString();
+    }
+
+    void AutoClick()
+    {
+        clickCounter += autoClickAmount;
+        clickCounterText.text = clickCounter.ToString();
+    }
+
+    void ToggleAutoClick()
+    {
+        isAutoClick = !isAutoClick; // Toggle the value
+    }
+
+    IEnumerator AutoClickCoroutine()
+    {
+        while (autoClickRunning)
+        {
+            yield return new WaitForSeconds(autoClickInterval);
+            AutoClick();
+        }
     }
 }
